5691f67881ecd97d8374c069d15b18bf59b5fb1f
edemorsy <edemorsy@linagora.com>
2013-03-01 16:18:53 +0100

    Ajout popup d'aide Corrections cosmétiques Gestion du nom d'utilisateur par cookie

--- gamegrinder/Gamegrinder/src/fr/neyrick/gamegrinder/jsf/Visitor.java
+++ gamegrinder/Gamegrinder/src/fr/neyrick/gamegrinder/jsf/Visitor.java
@@ -1,17 +1,36 @@
 package fr.neyrick.gamegrinder.jsf;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Instance;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.Cookie;
 
 @SuppressWarnings("serial")
 @Named
 @SessionScoped
 public class Visitor implements Serializable {
 
+	private static final String USER_COOKIE = "gamegrinder_user";
+	
 	private String name;
 	
+	@Inject
+	private Instance<FacesContext> facesContextInstance;
+	
+	@PostConstruct
+	private void init() {
+		Cookie cookie = (Cookie)facesContextInstance.get().getExternalContext().getRequestCookieMap().get(USER_COOKIE);
+		if (cookie != null) {
+			this.name = cookie.getValue();
+		}
+	}
 	
 	public String getName() {
 		return name;
@@ -19,6 +38,9 @@ public class Visitor implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+		Map<String, Object> cookieProps = new HashMap<String, Object>();
+		cookieProps.put("maxAge", new Integer(3600*24*100));
+		facesContextInstance.get().getExternalContext().addResponseCookie(USER_COOKIE, name, cookieProps);
 	}
 	
 	public boolean isKnown() {
